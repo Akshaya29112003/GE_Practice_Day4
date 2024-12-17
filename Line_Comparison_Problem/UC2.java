591089b5bf3ae3cab563ac4f3b479c572a9d7808
Akshaya <nalamakshaya04@gmail.com>
2024-12-17 15:46:30 +0530

    Add: UC2 Line Equality Check Implementation

---
+++ Line_Comparison_Problem/UC2.java
@@ -0,0 +1,10 @@
+package Line_Comparison_Problem;
+
+public class UC2 {
+    public static void equality_Of_Two_Lines(double length1, double length2){
+        Double num1 = length1;
+        Double num2 = length2;
+        if(num1.equals(num2))   System.out.println("2 lenghts are equal");
+        else    System.out.println("2 lengths are not equal");
+    }
+}
