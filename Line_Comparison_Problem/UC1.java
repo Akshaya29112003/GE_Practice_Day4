a1b0bbc65d08cd24132bee8d7aad0ab67f224c79
Akshaya <nalamakshaya04@gmail.com>
2024-12-17 15:39:00 +0530

    Add UC1 Cartesian System Implementation

---
+++ Line_Comparison_Problem/UC1.java
@@ -0,0 +1,21 @@
+package Line_Comparison_Problem;
+
+public class UC1 {
+
+    public static double cartesian_System(double x1, double y1, double x2, double y2){
+        double ans = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
+        return ans;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
